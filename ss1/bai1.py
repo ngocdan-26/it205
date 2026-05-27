ea60a7bd375e8c52a5a8d61664ec52a23f2135c2
ngocdan-26 <dongocdan2006@gmail.com>
2026-05-27 10:00:01 +0700

    Add files via upload

---
+++ ss1/bai1.py
@@ -0,0 +1,13 @@
+print(' --- HỆ THỐNG TIẾP NHẬN BỆNH NHÂN ---')
+name_patient = input('Nhập tên bệnh nhân: ')
+age = int(input('Mời bạn nhập tuổi: '))
+symptom = input('Mời bạn nhập triệu chứng bênh: ')
+print(' PHIẾU KHÁM BỆNH --- ')
+# bài là để dữ liệu trong print khum chính xác
+# print ('Tên bệnh nhân:', symptom);
+# print('Tuổi:', name_patient);
+# print ('Triệu chứng:', age);
+# sửa lại cho đúng
+print ('Tên bệnh nhân:', name_patient)
+print ('Tuổi:', age)
+print ('Triệu Chứng:', symptom)
---
+++ ss1/bai2.py
@@ -0,0 +1,8 @@
+print("- HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN -")
+name_patient = input("Nhập tên bệnh nhân: ")
+weight = float(input ("Nhập cân nặng bệnh nhân: "))
+print("- KIỂM TRA DỮ LIỆU LƯU TRỮ --")
+print("Bệnh nhân : ", name_patient)
+print("Cân nặng dã nhận :", weight)
+# Trường nhóm IT viết thêm dòng này để kiểm tra dữ liệu của cân nặng
+print("CẢNH BÁO Kiểu dữ liệu dang lưu là : " , type(weight))
